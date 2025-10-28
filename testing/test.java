52abe7c1c216e196202aec9ce4f45e7eefaed498
chhengsokuntheary572-cyber <chhengsokuntheary572@gmail.com>
2025-10-28 08:39:49 +0700

    Implement Number class for user input and countdown
    Added a Number class to process user input and count down.

--- testing/test.java
+++ testing/test.java
@@ -1,1 +1,25 @@
 #Testing java code
+  import java.util.Scanner;
+
+public class Number {
+    int num; 
+    public Number() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        num = input.nextInt();
+    }
+
+    public void processNumber() {
+        if (num > 1000) {
+            System.out.println("Please enter a number 1000 or less.");
+        } else if (num < 0) {
+            System.out.println("Please enter a positive number or zero.");
+        } else {
+            System.out.println("Counting down from " + num + " to 0:");
+            for (int i = num; i >= 0; i--) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
